04da1e198f18a48615815e920e3deac638295794
nawantorn113 <stu6512732113@sskru.ac.th>
2025-10-14 09:20:37 +0700

    commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,8 @@
 # mysite/urls.py
-
 from django.contrib import admin
-from django.urls import path, include # <-- ตรวจสอบว่ามี include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    # บรรทัดนี้จะบอกว่า "ถ้ามีใครเข้ามาที่เว็บ ให้ส่งต่อไปให้ booking/urls.py จัดการ"
-    path('', include('booking.urls')), 
+    path('', include('booking.urls')), # ให้แอป booking จัดการ URL ทั้งหมด
 ]
